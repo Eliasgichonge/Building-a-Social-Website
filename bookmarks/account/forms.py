d28292c371d4711cdfdd0fe76568e1faf98f3fab
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-28 15:00:32 +0300

    User registration

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -17,6 +17,6 @@ class UserRegistrationForm(forms.ModelForm):
 
       def clean_password2(self):
           cd = self.cleaned_data
-            if cd['password'] != cd['password2']:
+          if cd['password'] != cd['password2']:
                   raise forms.ValidationError('Passwords don\'t match.')
             return cd['password2']
