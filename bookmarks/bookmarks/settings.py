0917672ec87f296ac6151346f1594fae72cbcd42
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-28 16:00:24 +0300

    Installing Pillow and serving media files

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -115,9 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+# Static
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+# Media
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
