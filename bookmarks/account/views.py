a6e665f1be0a20c0864314667e7cdad9a05d01c6
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-28 07:49:36 +0300

    Creating a login view

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -4,6 +4,6 @@ from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 
 def user_login(request):
- if request.method == 'POST':
- form = LoginForm(request.POST)
- if form.is_valid():
+    if request.method == 'POST':
+       form = LoginForm(request.POST)
+       if form.is_valid():
