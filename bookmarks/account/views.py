0ff11762ba2244811052515aef9fc13cd2fbae60
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-28 15:31:48 +0300

    User registration

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,14 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
+from .forms import LoginForm
 
 @login_required
 def dashboard(request):
-    return render(request,
-                  'account/dashboard.html',
-                  {'section': 'dashboard'})
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
 
 def user_login(request):
     if request.method == 'POST':
@@ -16,16 +14,16 @@ def user_login(request):
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(request,
-                               username=cd['username'],
-                               password=cd['password'])
+                                username=cd['username'],
+                                password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authenticated successfully')
+                    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
                 else:
                     return HttpResponse('Disabled account')
             else:
                 return HttpResponse('Invalid login')
     else:
         form = LoginForm()
-        return render(request, 'account/login.html', {'form': form})
+    return render(request, 'account/login.html', {'form': form})
