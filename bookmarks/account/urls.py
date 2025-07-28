061f1c1da6625d3c836fbea41dc47105b9a4fc2e
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-28 13:42:16 +0300

    Change password views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
          auth_views.PasswordChangeView.as_view(),
          name='password_change'),
     path('password-change/done/',
-      auth_views.PasswordChangeDoneView.as_view(),
+         auth_views.PasswordChangeDoneView.as_view(),
       name='password_change_done'),
 ]
