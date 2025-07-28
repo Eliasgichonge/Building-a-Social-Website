4f3e734108e0a87c32f9637743443319335e1b0e
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-28 14:06:05 +0300

    Reset password views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
  auth_views.PasswordResetView.as_view(),
  name='password_reset'),
     path('password-reset/done/',
- auth_views.PasswordResetDoneView.as_view(),
+         auth_views.PasswordResetDoneView.as_view(),
  name='password_reset_done'),
     path('password-reset/<uidb64>/<token>/',
 auth_views.PasswordResetConfirmView.as_view(),
