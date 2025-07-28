e4f15ee8ced2178605d53607e7ae1def2e188872
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-28 14:03:21 +0300

    Reset password views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard'),
 
     # change password urls
     path('password-change/',
