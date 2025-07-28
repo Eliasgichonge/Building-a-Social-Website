8dbf8040151187c81e32fd98b345b6889bba1114
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-28 15:53:35 +0300

    Extending the user model

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -7,5 +7,5 @@ class Profile(models.Model):
       date_of_birth = models.DateField(blank=True, null=True)
       photo = models.ImageField(upload_to='users/%Y/%m/%d/',
                                 blank=True)
- def __str__(self):
+      def __str__(self):
  return f'Profile of {self.user.username}'
