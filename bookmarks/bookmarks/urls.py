c5b9047e0ff913d5d5c7af8f45a1b50f54907dd8
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-28 14:37:26 +0300

    Cleaning the codes

--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -21,4 +21,11 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+
+# ... other URLs
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
 ]
