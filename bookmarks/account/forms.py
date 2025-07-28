773d91961259c16c0dd5c77286f061399c914756
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-28 15:29:14 +0300

    User registration

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, UserRegistrationForm
-
-# If you plan to use LoginForm and UserRegistrationForm in a separate forms.py,
-# make sure not to redefine them here.
-# If not, you can define them below (but not both places).
 
 class LoginForm(forms.Form):
     username = forms.CharField()
@@ -27,18 +18,3 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError("Passwords don't match.")
         return cd['password2']
-
-def register(request):
-    if request.method == 'POST':
-        user_form = UserRegistrationForm(request.POST)
-        if user_form.is_valid():
-            # Create a new user object but avoid saving it yet
-            new_user = user_form.save(commit=False)
-            # Set the chosen password
-            new_user.set_password(user_form.cleaned_data['password'])
-            # Save the User object
-            new_user.save()
-            return render(request, 'account/register_done.html', {'new_user': new_user})
-    else:
-        user_form = UserRegistrationForm()
-    return render(request, 'account/register.html', {'user_form': user_form})
