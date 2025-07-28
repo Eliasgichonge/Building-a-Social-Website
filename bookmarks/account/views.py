ef4dd11ee8ef52a5e83c2fd24a5cf824260db2d0
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-28 15:42:33 +0300

    User registration

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -34,16 +34,15 @@ def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
+            # Create new user object but do not save immediately
             new_user = user_form.save(commit=False)
+            # Set password securely
             new_user.set_password(user_form.cleaned_data['password'])
+            # Save user to the database
             new_user.save()
-            # Optionally authenticate and login the user immediately after registration
-            user = authenticate(username=new_user.username,
-                                password=user_form.cleaned_data['password'])
-            if user:
-                login(request, user)
-                return redirect('dashboard')
+            # Show registration success page with welcome message
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
+
