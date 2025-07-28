9c63507cbc9ac86b74008aeaf2afc02d2f1af148
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-28 08:18:57 +0300

    Login and logout views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     # previous login url
     # path('login/', views.user_login, name='login'),
-# login / logout urls
+    # login / logout urls
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
