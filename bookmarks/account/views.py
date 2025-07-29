e956b396f7d62a1e2c3f49af0420ffc552e7d511
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-29 13:20:05 +0300

    Using the messages framework

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -64,6 +64,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated ' \
+                                      'successfully')
+            else:
+            messages.error(request, 'Error updating your profile')
             return redirect('dashboard')  # Optional success redirect
     else:
         user_form = UserEditForm(instance=request.user)
