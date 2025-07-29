11d649101ac0979881e8d312feb650599e7b4d26
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-29 14:14:32 +0300

    Running the development server through HTTPS

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -134,6 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTHENTICATION_BACKENDS = [
  'django.contrib.auth.backends.ModelBackend',
  'account.authentication.EmailAuthBackend',
+'social_core.backends.facebook.FacebookOAuth2',
 ]
 
 LOGIN_REDIRECT_URL = 'dashboard'
