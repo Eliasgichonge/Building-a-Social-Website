ab8ba142c8108b73c18af8a055b60182518d01b7
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-29 14:48:29 +0300

    Creating an image bookmarking website

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -5,8 +5,7 @@ class Image(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         related_name='images_created',
-        on_delete=models.CASCADE
-    )
+        on_delete=models.CASCADE    )
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
     url = models.URLField(max_length=2000)
