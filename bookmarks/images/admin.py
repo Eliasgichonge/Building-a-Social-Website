8f141da2930d00a7e3cf9352d89deaa6662afaf5
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-29 14:53:17 +0300

    Creating many-to-many relationships

--- bookmarks/images/admin.py
+++ bookmarks/images/admin.py
@@ -4,4 +4,4 @@ from .models import Image
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
       list_display = ['title', 'slug', 'image', 'created']
- list_filter = ['created']
+      list_filter = ['created']
