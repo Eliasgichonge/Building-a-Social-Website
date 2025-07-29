bcd000f3f9942bb6227fbf63cf6a6ba249cd5aee
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-29 13:46:14 +0300

    Adding social authentication to your site

--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -23,6 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+path('social-auth/',
+ include('social_django.urls', namespace='social')),
 
     # ... other URLs
     path('reset/<uidb64>/<token>/',
