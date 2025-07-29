017dd803f7382c22486ed87545c243e43c2700e0
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-29 10:14:02 +0300

    Creating migrations for the profile model

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
 from .forms import LoginForm, UserRegistrationForm, \UserEditForm, ProfileEditForm
 from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
