5f7f73f3f2a3a2490bf6c574e3e673e5da6feedc
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-29 14:55:51 +0300

    Posting content from other websites

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Image
 
 class ImageCreateForm(forms.ModelForm):
- class Meta:
+      class Meta:
  model = Image
  fields = ['title', 'url', 'description']
  widgets = {
