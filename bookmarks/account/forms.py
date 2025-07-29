630dedff093aec4bba69fa567d7f94385c05e1a2
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-29 10:06:57 +0300

    Creating migrations for the profile model

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -21,7 +21,7 @@ class UserRegistrationForm(forms.ModelForm):
         return cd['password2']
 
 class UserEditForm(forms.ModelForm):
- class Meta:
+      class Meta:
  model = User
  fields = ['first_name', 'last_name', 'email']
 class ProfileEditForm(forms.ModelForm):
