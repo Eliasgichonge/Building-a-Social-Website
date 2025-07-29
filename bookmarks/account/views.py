a51e6d4cc0d90c82ad64adb2eb3bda3cabd65a0c
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-29 12:49:19 +0300

    Creating migrations for the profile model

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -56,7 +56,7 @@ def edit(request):
                                 data=request.POST)
        profile_form = ProfileEditForm(instance=request.user.profile,
                                       data=request.POST,
- files=request.FILES)
+                                      files=request.FILES)
  if user_form.is_valid() and profile_form.is_valid():
  user_form.save()
  profile_form.save()
