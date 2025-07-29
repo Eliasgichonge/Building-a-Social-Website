38a7b4d29df17aad95ec0061b08dd934ef0654dc
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-29 14:58:01 +0300

    Posting content from other websites

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -13,7 +13,7 @@ def clean_url(self):
     url = self.cleaned_data['url']
     valid_extensions = ['jpg', 'jpeg', 'png']
     extension = url.rsplit('.', 1)[1].lower()
- if extension not in valid_extensions:
+    if extension not in valid_extensions:
  raise forms.ValidationError('The given URL does not ' \
  'match valid image extensions.')
  return url
