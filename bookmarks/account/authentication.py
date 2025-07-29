3c4b48252985d9f70a27d9acbf8bfe4fd7d12012
Elias M. Mwita <eliasmwita86@gmail.com>
2025-07-29 14:44:12 +0300

    Authentication using Google

--- bookmarks/account/authentication.py
+++ bookmarks/account/authentication.py
@@ -24,5 +24,5 @@ class EmailAuthBackend:
 def create_profile(backend, user, *args, **kwargs):
     """
     Create user profile for social authentication
- """
+    """
  Profile.objects.get_or_create(user=user)
