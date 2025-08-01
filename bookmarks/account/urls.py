66b5f42f294289d369fe6488c3dd91593d7b488d
Elias M. Mwita <eliasmwita86@gmail.com>
2025-08-01 16:36:56 +0300

    Using Django authentication views Login and logout views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
      # path('login/', views.user_login, name='login'),
 
      # login / logout urls
- path('login/', auth_views.LoginView.as_view(), name='login'),
- path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+     path('login/', auth_views.LoginView.as_view(), name='login'),
+     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
