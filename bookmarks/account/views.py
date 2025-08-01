18106cfeede733913a809b4752ce7a777d242b7d
Elias M. Mwita <eliasmwita86@gmail.com>
2025-08-01 16:51:47 +0300

    Using Django authentication views Login and logout views

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 def user_login(request):
     if request.method == 'POST':
